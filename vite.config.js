49b72f2afb2a6f1f5bd8e56855a2594823f2a136
nmelgar <nmelgar16013@gmail.com>
2024-10-12 19:13:44 -0600

    Small change to configs, in order to deploy.

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   root: 'src/',
 
   build: {
-    outDir: '../dist',
+    outDir: '../dist/client',
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'src/index.html'),
